c1ef399804622b090b2385c2e31f487b62086261
RubenRidissi <rridissi@gmail.com>
2026-06-22 21:42:33 -0300

    Update sw.js

--- Distrilst/sw.js
+++ Distrilst/sw.js
@@ -1,15 +1,33 @@
-const CACHE_NAME = "hojuelas-v1";
+const CACHE_NAME = "hojuelas-v2";
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         "/",
         "/index.html",
-        "/manifest.json"
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
       ]);
     })
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
